e9c7d5a21fc0331a0adf64930d75222e3a64d288
Mandresy Rabenja <mandresyrabenj@gmail.com>
2022-01-20 19:09:50 +0300

    fix: fix nodejs configuration to deploy this application

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
-const app = express;
-app.request(express.static(__dirname + '/dist/good-government'));
+const app = express();
+app.use(express.static(__dirname + '/dist/good-government'));
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + 'dist/good-government/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/good-government/index.html'));
 });
 app.listen(process.env.PORT || 8080);
