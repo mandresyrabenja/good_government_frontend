14767e57efdb48e8f0a94c7e65bcb808eda24a96
Mandresy Rabenja <mandresyrabenj@gmail.com>
2022-01-21 11:31:26 +0300

    feat: Desing navbar and sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -9,44 +9,8 @@ declare interface RouteInfo {
 export const ROUTES: RouteInfo[] = [
   {
     path: "/statistic",
-    title: "Statistic",
+    title: "Statistiques",
     icon: "icon-chart-pie-36",
-    class: ""
-  },
-  {
-    path: "/icons",
-    title: "Icons",
-    icon: "icon-atom",
-    class: ""
-  },
-  {
-    path: "/maps",
-    title: "Maps",
-    icon: "icon-pin",
-    class: "" },
-  {
-    path: "/notifications",
-    title: "Notifications",
-    icon: "icon-bell-55",
-    class: ""
-  },
-
-  {
-    path: "/user",
-    title: "User Profile",
-    icon: "icon-single-02",
-    class: ""
-  },
-  {
-    path: "/tables",
-    title: "Table List",
-    icon: "icon-puzzle-10",
-    class: ""
-  },
-  {
-    path: "/typography",
-    title: "Typography",
-    icon: "icon-align-center",
     class: ""
   }
 ];
