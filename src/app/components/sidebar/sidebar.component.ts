e7e2ff4a6d34d9b73093ab1ff448a2c16b44855d
Mandresy Rabenja <mandresyrabenj@gmail.com>
2022-01-28 08:02:10 +0300

    refactor: refactor sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -12,55 +12,58 @@ export const ROUTES: RouteInfo[] = [
     title: "Statistiques",
     icon: "icon-chart-pie-36",
     class: ""
-  },
-  {
-    path: "crud-region",
-    title: "Régions",
-    icon: "icon-world",
-    class: ""
-  },
+  }
+  // ,
+  // {
+  //   path: "crud-region",
+  //   title: "Régions",
+  //   icon: "icon-world",
+  //   class: ""
+  // }
+  ,
   {
     path: "assign-report",
     title: "Signalements",
     icon: "icon-bell-55",
     class: ""
-  },
-  {
-    path: "icons",
-    title: "Icons",
-    icon: "icon-atom",
-    class: ""
-  },
-  {
-    path: "maps",
-    title: "Maps",
-    icon: "icon-world",
-    class: ""
-  },
-  {
-    path: "notifications",
-    title: "Notification",
-    icon: "icon-bell-55",
-    class: ""
-  },
-  {
-    path: "user",
-    title: "User",
-    icon: "icon-single-02",
-    class: ""
-  },
-  {
-    path: "tables",
-    title: "Table",
-    icon: "icon-puzzle-10",
-    class: ""
-  },
-  {
-    path: "typography",
-    title: "Typography",
-    icon: "icon-align-center",
-    class: ""
   }
+  // ,
+  // {
+  //   path: "icons",
+  //   title: "Icons",
+  //   icon: "icon-atom",
+  //   class: ""
+  // },
+  // {
+  //   path: "maps",
+  //   title: "Maps",
+  //   icon: "icon-world",
+  //   class: ""
+  // },
+  // {
+  //   path: "notifications",
+  //   title: "Notification",
+  //   icon: "icon-bell-55",
+  //   class: ""
+  // },
+  // {
+  //   path: "user",
+  //   title: "User",
+  //   icon: "icon-single-02",
+  //   class: ""
+  // },
+  // {
+  //   path: "tables",
+  //   title: "Table",
+  //   icon: "icon-puzzle-10",
+  //   class: ""
+  // },
+  // {
+  //   path: "typography",
+  //   title: "Typography",
+  //   icon: "icon-align-center",
+  //   class: ""
+  // }
 ];
 
 @Component({
