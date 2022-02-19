f39a98787458e8ae35e7ea5417237cac4623f7c0
Mandresy Rabenja <mandresyrabenj@gmail.com>
2022-02-19 11:34:15 +0300

    feat: Add modal to modify the selected region's info

--- src/app/pages/crud-region/crud-region.component.ts
+++ src/app/pages/crud-region/crud-region.component.ts
@@ -1,6 +1,7 @@
 import { RegionService } from './../../services/regionservice';
 import { Component, OnInit } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-crud-region',
@@ -8,8 +9,12 @@ import { HttpErrorResponse } from '@angular/common/http';
 })
 export class CrudRegionComponent implements OnInit {
   regions : any[];
+  actualRegion : any;
+  modalReference: any;
+  closeResult: string;
 
-  constructor(private regionService : RegionService) { }
+  constructor(private regionService : RegionService,
+    private modalService: NgbModal) { }
 
   ngOnInit(): void {
 
@@ -24,4 +29,35 @@ export class CrudRegionComponent implements OnInit {
 
   }
 
+  /**
+   * Ouvrir le modal pour modifier un région
+   * @param index Indice du région au tableau des régions
+   * @param content Réference de l'html modal
+   */
+  modifyRegion(index : number, content) {
+    this.actualRegion = this.regions[index];
+    this.open(content);
+  }
+
+  open(content) {
+    this.modalReference = this.modalService.open(content, {centered: false, size: 'md'});
+    this.modalReference.result.then(
+      (result) => {
+        this.closeResult = `Closed with: ${result}`;
+      },
+      (reason) => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      }
+    );
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
 }
